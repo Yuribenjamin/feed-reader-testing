1c214de096dd1f1e53dd2f0a11068a40559dfab1
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2018-08-18 14:28:20 +0200

    test if Contain at least single entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -71,14 +71,20 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+   // test that ensures when the loadFeed function is called and completes its work, there is at least a single entry.
+    describe('Initial Entries', () => {
+        let feed = document.querySelector('.feed').getElementsByClassName('entry');
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        beforeEach((done)=> {
+            loadFeed(0, () => {
+                done();
+            });
+        });
+        it('Contain at least single entry', ((done) => {
+            expect(feed.length).toBeGreaterThan(0);
+            done();
+        }));
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
